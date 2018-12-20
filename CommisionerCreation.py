162ef866f13e344101a1736a3b90f25702916b56
Caleb Krlich <krlichc@yahoo.com>
2018-12-20 00:21:32 -0500

    added priority combobox and button commands

--- CommisionerCreation.py
+++ CommisionerCreation.py
@@ -9,15 +9,16 @@ class CommisionerWindow:
 
     def __init__(self):
         self.comOut = Commisioner("",0,"")
-
         self.createWindow()
-
+    
     def __del__(self):
         return self.comOut
 
-    def destroyWindow(self,window):
+    def destroyWindow(self,window,isReturning):
         window.quit()
         window.destroy()
+
+
         
     def createWindow(self):
         win = tk.Tk()
@@ -37,21 +38,25 @@ class CommisionerWindow:
         ttk.Entry(subFrame,width=12).grid(row=0,column=3)
 
         #Email Information
-        ttk.Label(subFrame,text="Email:").grid(row=2,column=0)
-        ttk.Entry(subFrame,width=12).grid(row=2,column=1)
+        ttk.Label(subFrame,text="Email:").grid(row=1,column=0)
+        ttk.Entry(subFrame,width=12).grid(row=1,column=1)
+        
+        #Priority Information
+        ttk.Label(subFrame,text="Priority Level:").grid(row=1,column=2)
+        ttk.Combobox(subFrame,width=9,values=["Low","Medium","High"]).grid(row=1,column=3)
 
+        #Notes Information
         ttk.Label(subFrame,text="Notes:").grid(row=3,column=0)
         scrolledtext.ScrolledText(win,width=25,height=10,wrap=tk.WORD).grid(row=1,column=0)
 
+        #Aligns buttons nicely
         buttonFrame = ttk.Frame(win)
         buttonFrame.grid(row=2,column=0)
 
-        ttk.Button(buttonFrame,text="Submit").grid(row=0,column=0)
-        ttk.Button(buttonFrame,text="Cancel").grid(row=0,column=1)
+        ttk.Button(buttonFrame,text="Submit",command=lambda: self.destroyWindow(win,True)).grid(row=0,column=0)
+        ttk.Button(buttonFrame,text="Cancel",command=lambda: self.destroyWindow(win,False)).grid(row=0,column=1)
 
         win.mainloop()
 
-
-#TESTS 
-
-testWindow = CommisionerWindow()
+test = CommisionerWindow()
+print(test.comOut.getName())
